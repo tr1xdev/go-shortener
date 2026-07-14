f39144692ca64998d1c9f2cf3e3bd2e12b9b303a
tr1xdev <55844996+tr1xdev@users.noreply.github.com>
2026-07-14 10:46:22 +0300

    feat: add base62 encoder util

---
+++ internal/util/base62.go
@@ -0,0 +1,30 @@
+package util
+
+import "strings"
+
+const base62Chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func encodeBase62(num int64) string {
+	if num == 0 {
+		return string(base62Chars[0]) // 'a'
+	}
+
+	var sb strings.Builder
+	base := int64(len(base62Chars)) // 62
+
+	// collect digits, least-significant first
+	if num > 0 {
+		remainder := num % base
+		sb.WriteByte(base62Chars[remainder])
+		num = num / base
+	}
+
+	// reverse to get correct order
+	encoded := sb.String()
+	runes := []rune(encoded)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+
+	return string(runes)
+}
