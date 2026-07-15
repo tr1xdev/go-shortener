51f96be25d958414a7024c1a0b1a52accbbc3480
tr1xdev <55844996+tr1xdev@users.noreply.github.com>
2026-07-15 06:22:45 +0300

    fix: improve error handling and response serialization in GetShorten

--- internal/handlers/get_shorten.go
+++ internal/handlers/get_shorten.go
@@ -2,8 +2,12 @@ package handlers
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type GetShortenResponse struct {
@@ -15,21 +19,23 @@ func (h *Handler) GetShorten(w http.ResponseWriter, r *http.Request) {
 
 	url, err := h.Rdb.Get(r.Context(), code).Result()
 	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, "specified key does not exists '%s', err: %v", code, err)
+		if errors.Is(err, redis.Nil) {
+			// key does not exists
+			http.Error(w, "short link not found", http.StatusNotFound)
+			return
+		}
+		// any other error (network issue, timeout, redis unavailable, etc.)
+		fmt.Printf("redis error: %v", err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
 		return
 	}
 
 	var payload GetShortenResponse
 	payload.URL = url
 
-	res, err := json.Marshal(payload)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "failed to serialize JSON: %v", err)
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(payload); err != nil {
+		log.Printf("failed to serialize JSON: %v", err)
 		return
 	}
-
-	w.WriteHeader(http.StatusOK)
-	w.Write(res)
 }
